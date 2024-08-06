a32fe82810ce74aa15c78383ad24d2d6fd64f239
froggie3 <yokkin.h@gmail.com>
2024-08-06 17:39:40 +0000

    a

--- bisect.c
+++ bisect.c
@@ -1,64 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_SIZE 16
+#define ARRAY_SIZE 20 
 
 // seq 10 99 | shuf | xargs -n 10 | sort | sed -E 's/ /, /g'
 
 int solve(int, int);
-int bisect_upper_bound(int *, int, int);
-int bisect_lower_bound(int *, int, int);
+int upper_bound(int *, int, int);
+int lower_bound(int *, int, int);
 
 int main(void)
 {
-    int x, result;
-    int target[ARRAY_SIZE] = {  5, 10, 11, 17, 33, 39, 40, 41, 46, 53, 
-                               54, 64, 75, 76, 93, 99 };
-    x = 9;
-    result = bisect_upper_bound(target, ARRAY_SIZE, x);
+    int target[ARRAY_SIZE] = {  5, 10, 11, 13, 17, 20, 21, 24, 30, 39, 
+                               40, 41, 46, 53, 54, 64, 75, 76, 93, 99 };
+    int x = 9;
 
     printf("x = %d\n", x);
     printf("arr =");
 
     for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
         printf(" %d", target[i]);
+    }
 
-    printf("\nresult = %d\n", result);
+    printf("\nfound = %d\n", upper_bound(target, ARRAY_SIZE, x));
 
     return 0;
 }
 
 
-int bisect_lower_bound(int target[], int length, int x)
+int lower_bound(int target[], int length, int x)
 {
     int lower = 0;
     int upper = length - 1;
 
-    while (abs(upper - lower) > 1) {
+    while (abs(upper - lower) > 1)
+    {
         int mid = lower + upper / 2;
-        if (solve(target[mid], x)) {
+
+        if (solve(target[mid], x))
+        {
             lower = mid;
-        } else {
+        }
+        else
+        {
             upper = mid;
         }
     }
+
     return lower;
 }
 
 
-int bisect_upper_bound(int target[], int length, int x)
+int upper_bound(int target[], int length, int x)
 {
     int lower = 0;
     int upper = length - 1;
 
-    while (abs(upper - lower) > 1) {
+    while (abs(upper - lower) > 1)
+    {
         int mid = lower + upper / 2;
-        if (solve(target[mid], x)) {
+
+        if (solve(target[mid], x))
+        {
             lower = mid;
-        } else {
+        }
+        else
+        {
             upper = mid;
         }
     }
+
     return upper;
 }
 
@@ -67,3 +79,4 @@ int solve(int compared, int x)
 {
     return compared == x;
 }
+
